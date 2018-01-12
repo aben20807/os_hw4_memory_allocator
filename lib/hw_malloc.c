a7ed6f7a4eb8c2dcd914a1e68669312d8d32c66d
Huang Po-Hsuan <aben20807@gmail.com>
2018-01-12 16:58:24 +0800

    Create chunk function

--- lib/hw_malloc.c
+++ lib/hw_malloc.c
@@ -4,7 +4,7 @@
 
 bool has_init = false;
 chunk_ptr_t start_sbrk = NULL;
-struct bin_t bin[7];
+struct bin_t bin[7] = {};
 
 void *hw_malloc(size_t bytes)
 {
@@ -14,12 +14,12 @@ void *hw_malloc(size_t bytes)
 		for (int i = 0; i < 7; i++) {
 			bin[i].prev = &bin[i];
 			bin[i].next = &bin[i];
-			printf("bin[%d].p: %p\n", i, bin[i].prev);
-			printf("bin[%d].n: %p\n", i, bin[i].next);
 		}
 		start_sbrk = (chunk_ptr_t)sbrk(64 * 1024);
-		printf("sbrk: %p\n", start_sbrk);
+		chunk_header *s = create_chunk(64 * 1024);
+		printf("sbrk: %p, size: %lli\n", start_sbrk, s->chunk_size);
 		if (64 * 1024 - chunk_size > 8) {
+			split();
 		} else {
 		}
 	} else {
@@ -39,6 +39,26 @@ void *get_start_sbrk(void)
 	return (void *)start_sbrk;
 }
 
-void put_in_bin(struct chunk_header c)
+static chunk_header *create_chunk(const chunk_size_t size)
 {
+	chunk_header *ret = calloc(1, sizeof(chunk_header));
+	ret->chunk_size = size;
+	ret->prev = NULL;
+	ret->next = NULL;
+	ret->pre_chunk_size = 0;
+	ret->prev_free_flag = 0;
+	return ret;
+}
+
+static void split()
+{
+}
+
+static void en_bin(struct chunk_header c)
+{
+}
+
+static chunk_header de_bin()
+{
+	// return NULL;
 }
--- lib/hw_malloc.h
+++ lib/hw_malloc.h
@@ -9,6 +9,7 @@
 typedef void *chunk_ptr_t;
 typedef long long chunk_size_t;
 typedef long long chunk_flag_t;
+typedef struct chunk_header chunk_header;
 
 struct chunk_header {
 	chunk_ptr_t prev;
@@ -31,5 +32,10 @@ extern struct bin_t bin[7];
 extern void *hw_malloc(size_t bytes);
 extern int hw_free(void *mem);
 extern void *get_start_sbrk(void);
+
+static chunk_header *create_chunk(const chunk_size_t size);
+static void split();
+static void en_bin(struct chunk_header c);
+static struct chunk_header de_bin();
 
 #endif
