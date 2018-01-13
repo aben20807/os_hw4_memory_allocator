3a5ed5c6147b74ec7449eaf791928ed1b206a1b9
Huang Po-Hsuan <aben20807@gmail.com>
2018-01-13 14:52:16 +0800

    Search for suitable bin for de_bin, fixed bug in create_chunk, can split, ac three easy test

--- lib/hw_malloc.c
+++ lib/hw_malloc.c
@@ -10,13 +10,14 @@ bin_t s_bin[7] = {};
 bin_t *bin[7];
 /*Static function*/
 static chunk_header *create_chunk(const chunk_size_t size);
-static chunk_header *split(chunk_header *ori, const chunk_size_t need);
+static chunk_header *split(chunk_header **ori, const chunk_size_t need);
+static int search_bin(const chunk_size_t need);
 static void en_bin(const int bin_num, chunk_header *c_h);
 static chunk_header *de_bin(const int index, const chunk_size_t need);
 
 void *hw_malloc(size_t bytes)
 {
-	long long chunk_size = bytes + 40LL;
+	long long need = bytes + 40LL;
 	if (!has_init) {
 		has_init = true;
 		for (int i = 0; i < 7; i++) {
@@ -30,41 +31,39 @@ void *hw_malloc(size_t bytes)
 		chunk_header *s = create_chunk(64 * 1024);
 		heap_brk = start_brk; // reset heap top pointer
 		printf("sbrk: %p, size: %lli\n", start_brk, s->chunk_size);
-		if (64 * 1024 - chunk_size > 8) {
-			chunk_header *c = split(s, chunk_size);
-			en_bin(6, s);
-			return (void *)((intptr_t)(void*)c +
-			                sizeof(chunk_header) -
-			                (intptr_t)(void*)start_brk);
-		} else {
-			return (void *)((intptr_t)(void*)s +
-			                sizeof(chunk_header) -
-			                (intptr_t)(void*)start_brk);
-		}
+		// if (64 * 1024 - need > 8) {
+		chunk_header *c = split(&s, need);
+		return (void *)((intptr_t)(void*)c +
+		                sizeof(chunk_header) -
+		                (intptr_t)(void*)start_brk);
+		// } else {
+		// return (void *)((intptr_t)(void*)s +
+		// sizeof(chunk_header) -
+		// (intptr_t)(void*)start_brk);
+		// }
 	} else {
 		chunk_header *r = NULL;
-		if (chunk_size <= 48) {
-			r = de_bin(0, chunk_size);
-		} else if (chunk_size <= 56) {
-			r = de_bin(1, chunk_size);
-		} else if (chunk_size <= 64) {
-			r = de_bin(2, chunk_size);
-		} else if (chunk_size <= 72) {
-			r = de_bin(3, chunk_size);
-		} else if (chunk_size <= 80) {
-			r = de_bin(4, chunk_size);
-		} else if (chunk_size <= 88) {
-			r = de_bin(5, chunk_size);
-		} else { // > 88
-			// r = de_bin(6, chunk_size);
-		}
-		if (r == NULL) {
-			printf("not found in bin\n");
-		} else {
+		int bin_num = search_bin(need);
+		if (bin_num == -1) {
+		} else if (bin_num <= 5) {
+			r = de_bin(bin_num, need);
 			return (void *)((intptr_t)(void*)r +
 			                sizeof(chunk_header) -
 			                (intptr_t)(void*)start_brk);
+		} else { // bin_num = 6
+			chunk_header *s = de_bin(6, need);
+			chunk_header *c = split(&s, need);
+			return (void *)((intptr_t)(void*)c +
+			                sizeof(chunk_header) -
+			                (intptr_t)(void*)start_brk);
 		}
+		//     if (r == NULL) {
+		//         printf("not found in bin\n");
+		//     } else {
+		//         return (void *)((intptr_t)(void*)r +
+		//                         sizeof(chunk_header) -
+		//                         (intptr_t)(void*)start_brk);
+		//     }
 	}
 	return NULL;
 }
@@ -79,15 +78,15 @@ void *get_start_sbrk(void)
 	return (void *)start_brk;
 }
 
-static chunk_header *create_chunk(const chunk_size_t size)
+static chunk_header *create_chunk(const chunk_size_t need)
 {
-	if (heap_brk - start_brk + size > 64 * 1024) {
+	if (heap_brk - start_brk + need > 64 * 1024) {
 		printf("heap not enough\n");
 		return NULL;
 	}
 	chunk_header *ret = heap_brk;
-	heap_brk += (sizeof(chunk_header) + size);
-	ret->chunk_size = size;
+	heap_brk += need;
+	ret->chunk_size = need;
 	ret->prev = NULL;
 	ret->next = NULL;
 	ret->pre_chunk_size = 0;
@@ -95,13 +94,41 @@ static chunk_header *create_chunk(const chunk_size_t size)
 	return ret;
 }
 
-static chunk_header *split(chunk_header *ori, const chunk_size_t need)
+static chunk_header *split(chunk_header **ori, const chunk_size_t need)
 {
-	ori->chunk_size -= need;
-	ori->pre_chunk_size = need;
-	ori->prev_free_flag = 0;
-	chunk_header *ret = create_chunk(need);
-	return ret;
+	printf("ori %p size: %lld, need: %lld\n", (*ori), (*ori)->chunk_size, need);
+	if ((*ori)->chunk_size - need >= 8) {
+		// printf("%p, %p\n", *ori, heap_brk);
+		chunk_header *new = (void *)((intptr_t)(void*)*ori + need);
+		new->chunk_size = (*ori)->chunk_size - need;
+		new->pre_chunk_size = need;
+		new->prev_free_flag = 0;
+		*ori = new;
+		chunk_header *ret = create_chunk(need);
+		en_bin(6, (*ori));
+		printf("en %p size: %lld\n", (*ori), (*ori)->chunk_size);
+		return ret;
+	} else {
+		return (*ori);
+	}
+}
+
+static int search_bin(const chunk_size_t need)
+{
+	for (int i = 0; i <= 5; i++) {
+		if (bin[i]->size == 0) {
+			continue;
+		}
+		if (need <= 40 + (i + 1) * 8) {
+			return i;
+		}
+	}
+	if (bin[6]->size > 0) {
+		return 6;
+	} else {
+		printf("not any free chunk\n");
+		return -1;
+	}
 }
 
 static void en_bin(const int index, chunk_header *c_h)
@@ -182,7 +209,8 @@ static chunk_header *de_bin(const int index, const chunk_size_t need)
 		case 6:
 			if (bin[6]->size > 0 &&
 			    need > ((chunk_header *)bin[6]->next)->chunk_size) {
-				printf("not enough\n");
+				printf("not enough bin: %d, %lld, need: %lld\n", bin[6]->size,
+				       ((chunk_header *)bin[6]->next)->chunk_size, need);
 				return NULL;
 			} else {
 				cur = bin[6]->prev;
