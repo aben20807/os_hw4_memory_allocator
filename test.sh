dc6e27a1406455278d7f45cfb27868793d4ec450
Huang Po-Hsuan <aben20807@gmail.com>
2018-01-13 11:24:16 +0800

    Update test script

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
-make
-time ./hw4_mm_test
 make clean
+m
+echo -e '\033[31m ╔══════════════════════════════╗'
+echo -n ' ║ '
+echo -n `date`
+echo    ' ║ '
+echo -e '\033[31m ╚══════════════════════════════╝'
+echo -e '\033[0m'
+./hw4_mm_test
