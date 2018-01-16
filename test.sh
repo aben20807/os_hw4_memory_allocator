e44544fc476d924fa1018a47df3b669666fa151c
Huang Po-Hsuan <aben20807@gmail.com>
2018-01-16 11:24:07 +0800

    Update test.sh, add test file and solution

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 make clean
-m
+make
 echo -e '\033[31m ╔══════════════════════════════╗'
 echo -n ' ║ '
 echo -n `date`
 echo    ' ║ '
 echo -e '\033[31m ╚══════════════════════════════╝'
 echo -e '\033[0m'
-./hw4_mm_test
+cat testfile.txt | ./hw4_mm_test > outputfile.txt
+if cmp -s outputfile.txt solution.txt ; then
+    echo "same"
+else
+    diff outputfile.txt solution.txt
+fi
