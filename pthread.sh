30c159a60db0ef683d8c46da25900d2f97d3def6
Huang Po-Hsuan <aben20807@gmail.com>
2018-01-18 21:31:49 +0800

    pthread not work QuQ

--- pthread.sh
+++
@@ -1,5 +0,0 @@
-cd lib/
-make
-cd ..
-gcc -std=gnu99 -I./lib -Wall -pthread -c hw4_mm_pthread_test.c -o hw4_mm_pthread_test.o
-gcc -std=gnu99 -I./lib -Wall -pthread -o hw4_mm_pthread_test hw4_mm_pthread_test.o ./lib/hw_malloc.o
